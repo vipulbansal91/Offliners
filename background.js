94286733648ad3893c1a8d4146a5f6091ecd6af6
Avijit Gupta <526avijit@gmail.com>
2017-04-02 18:45:52 +0530

    indendtation fixes, ui

--- background.js
+++ background.js
@@ -4,11 +4,11 @@ var trackedDownloadIds = [];
 var parentUrls = [];
 
 Array.prototype.forEachDone = function(fn, scope, lastfn) {
-    for(var i = 0, c = 0, len = this.length; i < len; i++) {
-        fn.call(scope, this[i], i, this, function() {
-            ++c === len && lastfn();
-        });
-    }
+  for(var i = 0, c = 0, len = this.length; i < len; i++) {
+    fn.call(scope, this[i], i, this, function() {
+      ++c === len && lastfn();
+    });
+  }
 };
 
 chrome.runtime.onMessage.addListener(
@@ -23,91 +23,91 @@ chrome.runtime.onMessage.addListener(
 
     request.links.forEachDone(function(link, i, arr, done){
 
-    	console.log(link);
+      console.log(link);
 
-    	chrome.storage.local.get('data', function(data) {
+      chrome.storage.local.get('data', function(data) {
 
-    		if (link.includes("w.amazon.com") && (link.includes("&action=edit&section=")) == false) {
-    			found = false;
+        if (link.includes("w.amazon.com") && (link.includes("&action=edit&section=")) == false) {
+          found = false;
 
-				  console.log(data.data);
+          console.log(data.data);
           if (data.data) {
-				    data.data.forEach(function(linkObject, index) {
+            data.data.forEach(function(linkObject, index) {
 
-					    if (linkObject.url === link) {
-						    found = true;
-					    }
-				    });
+              if (linkObject.url === link) {
+                found = true;
+              }
+            });
           }
 
 
-				console.log(found);
+          console.log(found);
 
-				if (found === false) {
-					linksToSave.push(link);
-				}
+          if (found === false) {
+            linksToSave.push(link);
+          }
 
-				console.log(linksToSave);
-    		}
+          console.log(linksToSave);
+        }
 
-    		done()
+        done()
 
-		});
+      });
 
 
-	}, this, function() {
-	    console.log(linksToSave);
-	    
-	    linksToSave.forEach(function(link, index) {
-    		savePages(link);
-    	});
-	});
+    }, this, function() {
+      console.log(linksToSave);
+
+      linksToSave.forEach(function(link, index) {
+        savePages(link);
+      });
+    });
 
 
   });
 
 
 function createLinkObjectToStore(url, filename, downloadId, pageTitle, isParent) {
-	return {
-		url: url,
-		name: url,
-		location: filename,
-		downloadId: downloadId,
+  return {
+    url: url,
+    name: url,
+    location: filename,
+    downloadId: downloadId,
     pageTitle: pageTitle,
-		isParent : isParent
-	};
+    isParent : isParent
+  };
 }
 
 function saveLinkToStorage(linkObj) {
-	chrome.storage.local.get('data', function(res) {
-		console.log(res.data);
-		if (res && res.data) {
-			result = res.data;
-		} else {
-			result = [];
-		}
-		result.push(linkObj);
-		chrome.storage.local.set({'data': result});
-	});
+  chrome.storage.local.get('data', function(res) {
+    console.log(res.data);
+    if (res && res.data) {
+      result = res.data;
+    } else {
+      result = [];
+    }
+    result.push(linkObj);
+    chrome.storage.local.set({'data': result});
+  });
 }
 
 function savePages(tabUrl) {
-	chrome.tabs.create({'url': tabUrl, 'active': false}, function(tab) {
-		tabsToSave.push(tab.id);
-		tabsToClose.push(tab.id);
-	});
+  chrome.tabs.create({'url': tabUrl, 'active': false}, function(tab) {
+    tabsToSave.push(tab.id);
+    tabsToClose.push(tab.id);
+  });
 }
 
 function searchAndGetURLFromStore(url, tabId) {
   chrome.storage.local.get('data', function(data) {
     if (data.data) {
-	    data.data.some(function(linkObj) {
-		    if (linkObj.url === url) {
+      data.data.some(function(linkObj) {
+        if (linkObj.url === url) {
           console.log('URL Matched, linkObj: ', linkObj);
           openFileURLInTab("file://" + linkObj.filename, tabId);
           return true;
-		    }
-	    });
+        }
+      });
     }
   });
 }
@@ -131,105 +131,105 @@ chrome.tabs.onUpdated.addListener(function(tabId , info, tab) {
   } else {
     console.log('Online');
   }
-    if (info.status == "complete") {
-        // your code ...
-        var tabUrl = tab.url;
-        console.log(tabUrl + " got fully loaded");
+  if (info.status == "complete") {
+    // your code ...
+    var tabUrl = tab.url;
+    console.log(tabUrl + " got fully loaded");
 
-      console.log('tabsToSave', 'tabsToClose');
-      console.log('Got tabUrl: ', tabUrl);
-      console.log(tabsToSave.toString(), tabsToClose.toString());
-        if (tabsToSave.indexOf(tabId) > -1) {
+    console.log('tabsToSave', 'tabsToClose');
+    console.log('Got tabUrl: ', tabUrl);
+    console.log(tabsToSave.toString(), tabsToClose.toString());
+    if (tabsToSave.indexOf(tabId) > -1) {
 
-        	tabsToSave.splice(tabsToSave.indexOf(tabId),1);
+      tabsToSave.splice(tabsToSave.indexOf(tabId),1);
 
-        	chrome.pageCapture.saveAsMHTML({
-	        	tabId: tab.id
-		      }, 	function(blob) {
-            var pageTitle = stripTitle(tab.title);
-            console.log('pageTitle: ', pageTitle);
-		        var url = URL.createObjectURL(blob);
-				//console.log('url: ', url);
-				  var filename = 'Offliners/' + Math.random().toString(36).slice(2) + '.mhtml';
+      chrome.pageCapture.saveAsMHTML({
+        tabId: tab.id
+      },  function(blob) {
+        var pageTitle = stripTitle(tab.title);
+        console.log('pageTitle: ', pageTitle);
+        var url = URL.createObjectURL(blob);
+        //console.log('url: ', url);
+        var filename = 'Offliners/' + Math.random().toString(36).slice(2) + '.mhtml';
 
-		        chrome.downloads.download({
-		            url: url,
-		            filename: filename
-		        }, function(downloadId) {
+        chrome.downloads.download({
+          url: url,
+          filename: filename
+        }, function(downloadId) {
 
-		        	if (tabsToClose.indexOf(tabId) > -1) {
+          if (tabsToClose.indexOf(tabId) > -1) {
 
-		        		chrome.tabs.remove(tab.id); // to close the tab
-		        		tabsToClose.splice(tabsToClose.indexOf(tabId),1);
-		        	}
+            chrome.tabs.remove(tab.id); // to close the tab
+            tabsToClose.splice(tabsToClose.indexOf(tabId),1);
+          }
 
-              		console.log('Saving to storage: ', tabUrl, filename);
+          console.log('Saving to storage: ', tabUrl, filename);
 
-		        	saveLinkToStorage(createLinkObjectToStore(tabUrl, filename, downloadId, pageTitle, isUrlParent(tabUrl)));
-		        });
-		    });
-        }
-
-        return;
+          saveLinkToStorage(createLinkObjectToStore(tabUrl, filename, downloadId, pageTitle, isUrlParent(tabUrl)));
+        });
+      });
     }
+
+    return;
+  }
 });
 
 function isUrlParent(url) {
-	isParent = false
+  isParent = false
 
-	if (parentUrls.indexOf(url) > -1) {
-		isParent = true;
+  if (parentUrls.indexOf(url) > -1) {
+    isParent = true;
 
-		parentUrls.splice(parentUrls.indexOf(url), 1);
-	}
+    parentUrls.splice(parentUrls.indexOf(url), 1);
+  }
 
-	return isParent;
+  return isParent;
 }
 
 chrome.downloads.onChanged.addListener(function(downloadDelta) {
-	if (downloadDelta.state && downloadDelta.state.current == "complete") {
-			chrome.downloads.search({
-				id: downloadDelta.id
-			}, function(results) {
-				updateFileNameInLocalStorage(downloadDelta.id, results[0].filename);
-			});
-	}
+  if (downloadDelta.state && downloadDelta.state.current == "complete") {
+    chrome.downloads.search({
+      id: downloadDelta.id
+    }, function(results) {
+      updateFileNameInLocalStorage(downloadDelta.id, results[0].filename);
+    });
+  }
 });
 
 
 function getDownloadFileName(downloadId) {
-	chrome.downloads.search({
-		id: downloadId
-	}, function(results) {
-		console.log(results[0]);
-		console.log(results[0].filename);
-		return results[0].filename;
-	});
+  chrome.downloads.search({
+    id: downloadId
+  }, function(results) {
+    console.log(results[0]);
+    console.log(results[0].filename);
+    return results[0].filename;
+  });
 }
 
 function updateFileNameInLocalStorage(downloadId, filename) {
-	console.log('Updating location for file: ', downloadId + " " + filename);
+  console.log('Updating location for file: ', downloadId + " " + filename);
 
-	chrome.storage.local.get('data', function(data) {
+  chrome.storage.local.get('data', function(data) {
     console.log('data: ', data.data);
     if (data.data) {
-		data.data.some(function(linkObject, index) {
-      // console.log('Found');
-			if (linkObject.downloadId == downloadId) {
-				console.log(linkObject.downloadId + " " + filename);
-				linkObject.filename = filename;
-        return true;
-			}
-		});
+      data.data.some(function(linkObject, index) {
+        // console.log('Found');
+        if (linkObject.downloadId == downloadId) {
+          console.log(linkObject.downloadId + " " + filename);
+          linkObject.filename = filename;
+          return true;
+        }
+      });
     }
 
-		console.log(data.data);
+    console.log(data.data);
 
-		chrome.storage.local.set({'data': data.data});
+    chrome.storage.local.set({'data': data.data});
 
-	});
+  });
 }
 
 function flushHttpOrHttps(url) {
-	return url.substring(url.indexOf(":") + 1);
+  return url.substring(url.indexOf(":") + 1);
 }
--- myfile.js
+++ myfile.js
@@ -7,46 +7,46 @@ var title = document.getElementById("articleTitle");
 // document.querySelector('articleTitle')
 title.innerHTML = '<button id="saveOfflineButton">Save</button>' + title.innerHTML;
 document.getElementById('saveOfflineButton').addEventListener('click', function(event) {
-	// alert('Clicked me');
-	var nestedLinks = document.querySelectorAll('#articleContent a');
-	var linksResult = [];
-	linksResult.push(document.location.href);
+  // alert('Clicked me');
+  var nestedLinks = document.querySelectorAll('#articleContent a');
+  var linksResult = [];
+  linksResult.push(document.location.href);
 
-	nestedLinks.forEach(function(link) {
-		if (linksResult.indexOf(link.href) === -1) {
-			linksResult.push(link.href);
-		}
-	});
+  nestedLinks.forEach(function(link) {
+    if (linksResult.indexOf(link.href) === -1) {
+      linksResult.push(link.href);
+    }
+  });
 
-	linksResult = filterHash(linksResult);
+  linksResult = filterHash(linksResult);
 
-	var data = {links: linksResult};
-	// alert('Sending message: ', data.nestedLinks);
-	console.log(data);
-	chrome.runtime.sendMessage(data, function(response) {
-		//alert('done');
-	});
+  var data = {links: linksResult};
+  // alert('Sending message: ', data.nestedLinks);
+  console.log(data);
+  chrome.runtime.sendMessage(data, function(response) {
+    //alert('done');
+  });
 });
 
 function filterHash(links) {
-	var newLinks = [];
+  var newLinks = [];
 
-	links.forEach(function(link, index) {
-		if (link.indexOf('#') > -1) {
-			link = link.substring(0, link.indexOf('#'));
-		}
+  links.forEach(function(link, index) {
+    if (link.indexOf('#') > -1) {
+      link = link.substring(0, link.indexOf('#'));
+    }
 
-		newLinks.push(link)
-	});
+    newLinks.push(link)
+  });
 
-	return newLinks.filter(onlyUnique);
+  return newLinks.filter(onlyUnique);
 }
 
-function onlyUnique(value, index, self) { 
-    return self.indexOf(value) === index;
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
 }
 
 
 // chrome.runtime.sendMessage({links: linksResult}, function(response) {
-// 	alert('done');
+//  alert('done');
 // });
